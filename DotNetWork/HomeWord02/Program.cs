74e89984e51c0dce8c55e4004680d3ff473f025e
HWeiney <111518087+HWeiney@users.noreply.github.com>
2022-09-19 22:49:36 +0800

    Add files via upload

---
+++ DotNetWork/HomeWord02/Program.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * 判断101-200之间有多少个素数，并输出所有素数。
+ * 程序分析：判断素数的方法：用一个数分别去除2到sqrt(这个数)，
+ * 如果能被整除，则表明此数不是素数，反之是素数。
+*/
+using System;
+
+namespace HomeWord02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 101; i <= 200; i++) // 质数从2开始到100结束
+            {
+                bool flag = true;
+                for (int j = 2; j < i; j++) // 能被2和到自身的前一个数整除
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    sum++;
+                    //Console.WriteLine(sum+ ":"+i + "是质数！");
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ DotNetWork/HomeWork01/Program.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * 有一对兔子，从出生后第3个月起每个月都生一对兔子，
+ * 小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子对数为多少？
+ * 程序分析： 兔子的规律为数列1,1,2,3,5,8,13,21....
+ * */
+using System;
+
+namespace HomeWork01
+{
+    class Program
+    {
+        static int NewRabbitOfMonth(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else if (n == 2)
+            {
+                return 0;
+            }
+            else
+            {
+                return NewRabbitOfMonth(n - 1) + NewRabbitOfMonth(n - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第几个月");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i = 1;i <= n; i++)
+            {
+                sum += NewRabbitOfMonth(i);
+            }
+            Console.Write("第" + n + "个月共有" + sum + "对兔子");
+        }
+    }
+}
