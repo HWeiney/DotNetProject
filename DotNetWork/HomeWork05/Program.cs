6029349c1e390ca907d842004773219bcc4ebf9e
HWeiney <111518087+HWeiney@users.noreply.github.com>
2022-09-20 09:39:35 +0800

    Add files via upload

---
+++ DotNetWork/HomeWork05/Program.cs
@@ -0,0 +1,29 @@
+﻿/**
+ *  输入一个学生成绩，输出成绩等级：学习成绩>=90分的同学用A表示，
+ *  60-89分之间的用B表示，60分以下的用C表示。
+ */
+using System;
+
+namespace HomeWork05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入学生成绩:");
+            int grade = int.Parse(Console.ReadLine());
+            if (grade >= 90)
+            {
+                Console.WriteLine("成绩等级：A");
+            }
+            else if(grade>=60 && grade <= 89)
+            {
+                Console.WriteLine("成绩等级：B");
+            }
+            else
+            {
+                Console.WriteLine("成绩等级：C");
+            }
+        }
+    }
+}
---
+++ DotNetWork/HomeWork06/Program.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * 输入两个正整数m和n，求其最大公约数和最小公倍数。
+ */
+using System;
+
+namespace HomeWork06
+{
+    class Program
+    {
+        static int MaxNum(int m,int n)
+        {
+            if (m < n)
+            {
+                int temp = m;
+                m = n;
+                n = temp;
+            }
+            int r = 1;
+            while (r != 0)
+            {
+                r = m % n;
+                m = n;
+                n = r;
+            }
+            return m;
+
+        }
+
+        static int MinNum(int m,int n)
+        {
+            return m * n / MaxNum(m, n);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入正整数m和n: ");
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("最大公约数是：" + MaxNum(m, n));
+            Console.WriteLine("最小公倍数是：" + MinNum(m, n));
+        }
+    }
+}
