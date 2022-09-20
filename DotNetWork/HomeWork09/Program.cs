1b61b99d3e718620f5688e44e8c455de900ff018
HWeiney <111518087+HWeiney@users.noreply.github.com>
2022-09-20 09:52:39 +0800

    Add files via upload

---
+++ DotNetWork/HomeWork09/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * 一球从100米高度自由落下，每次落地后反跳回原高度的一半；
+ * 再落下，求它在 第10次落地时，共经过多少米？第10次反弹多高？
+ */
+using System;
+
+namespace HomeWork09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			float h, s;
+			h = s = 100;
+			h = h / 2;
+			for (int i = 2; i <= 10; i++)
+			{
+				s = s + 2 * h;
+				h = h / 2;
+			}
+			Console.WriteLine("第10次落地时，共经过{0}米，第10次反弹高{1}米\n", s, h);
+		}
+    }
+}
---
+++ DotNetWork/HomeWork10/Program.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * 有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+ */
+using System;
+
+namespace HomeWork10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 1; i <= 4; i++) //三层循环，分别是百位，十位，个位。
+            {
+                int bw = i;
+                for (int j = 1; j <= 4; j++)
+                {
+                    int sw = j;
+                    if (bw != sw)
+                    {
+                        for (int k = 1; k <= 4; k++)
+                        {
+                            int gw = k;
+                            if (gw != bw && gw != sw)
+                            {
+                                count++;
+                                Console.WriteLine("{0}{1}{2}", bw, sw, gw);
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("一共有：" + count + "个");
+            Console.Read();
+        }
+    }
+}
